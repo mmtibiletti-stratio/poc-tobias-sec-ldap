d999ca5277ad21d8fc2d2cbb277ce135e6bd3f4c
xi319305 <martin.miguel@extern.isban.com>
2016-02-11 08:51:06 +0100

    Modificación HomeController

--- poc-tobias-sec-ldap/src/main/java/hello/HomeController.java
+++ poc-tobias-sec-ldap/src/main/java/hello/HomeController.java
@@ -8,7 +8,7 @@ public class HomeController {
 
     @RequestMapping("/")
     public String index() {
-        return "Welcome to the home page!";
+        return "Welcome to the private resource!";
     }
 
 }
